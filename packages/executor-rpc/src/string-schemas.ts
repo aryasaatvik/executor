c84ed91aeb84332beb2bd751091e3787ec9040c5
Saatvik Arya <aryasaatvik@gmail.com>
2026-03-30 06:26:13 +0530

    chore(rpc)!: remove @executor/rpc package
    The Effect-TS RPC protocol layer is being removed in favor of HTTP/REST
    API. All RPC handlers have been migrated to the API layer in
    apps/cli/src/server/api/.

--- packages/executor-rpc/src/string-schemas.ts
+++
@@ -1,9 +0,0 @@
-import * as Schema from "effect/Schema";
-
-export const TrimmedNonEmptyStringSchema = Schema.Trim.pipe(
-  Schema.nonEmptyString(),
-);
-
-export const OptionalTrimmedNonEmptyStringSchema = Schema.optional(
-  TrimmedNonEmptyStringSchema,
-);
