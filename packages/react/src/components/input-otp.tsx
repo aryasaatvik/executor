6570efa8e0a4a5c4eccdf2a9aa4100c21c313e0a
Saatvik Arya <aryasaatvik@gmail.com>
2026-09-13 18:02:35 +0530

    fix(react): preserve component type contracts

--- packages/react/src/components/input-otp.tsx
+++ packages/react/src/components/input-otp.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { OTPInput, OTPInputContext } from "input-otp";
+import { OTPInput, OTPInputContext, type RenderProps } from "input-otp";
 import { MinusIcon } from "lucide-react";
 
 import { cn } from "../lib/utils";
@@ -36,7 +36,7 @@ function InputOTPSlot({
 }: React.ComponentProps<"div"> & {
   index: number;
 }) {
-  const inputOTPContext = React.useContext(OTPInputContext);
+  const inputOTPContext = React.useContext<RenderProps>(OTPInputContext);
   const { char, hasFakeCaret, isActive } = inputOTPContext?.slots[index] ?? {};
 
   return (
